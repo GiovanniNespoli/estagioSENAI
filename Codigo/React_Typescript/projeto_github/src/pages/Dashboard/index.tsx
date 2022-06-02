4383633f568c2b835304fda7d1986bfff15f5eb1
Giovanni Nespoli <ggnespoli@gmail.com>
2022-06-02 16:53:53 -0300

    codigo

---
+++ Codigo/React_Typescript/projeto_github/src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+/**
+ * Arrow function ajuda na tipagem 
+ * FC -> Function component
+ */
+const Dashboard : React.FC = () => 
+{
+    return <h1>Explore repositorio no GitHub</h1>
+}
+
+export default Dashboard;
