3c12d7db5c8a7112c7de614a36f8dc354a36df81
Giovanni Nespoli <ggnespoli@gmail.com>
2022-06-14 11:08:22 -0300

    refactor: add new folder nodeAvancado

---
+++ Codigo/nodeAvancado/src/modules/appointments/infra/typeorm/repositories/appointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm'
+
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+
+import Appointment from "@modules/appointments/infra/typeorm/entities/Appointments";
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+
+export default class AppointmentsRepository implements IAppointmentRepository{
+    private ormRepository: Repository<Appointment>;
+
+    constructor()
+    {
+        this.ormRepository = getRepository(Appointment);
+    }
+    /**
+     * Toda vez em que criamos uma função async await
+     * o retorno vira uma promise => Promise<Appointments | null>
+     * .then(response => )
+     */
+    public async findByDate(date : Date) : Promise<Appointment | undefined>
+    {
+        const findDate = await this.ormRepository.findOne({
+            where : { date },
+        });
+
+        return findDate;
+    }
+
+    public async create({provider_id , date}: ICreateAppointmentDTO): Promise<Appointment> {
+        const appointment =  this.ormRepository.create({provider_id, date});
+
+        await this.ormRepository.save(appointment);
+
+        return appointment;
+    }
+
+};
---
+++ Codigo/nodeAvancado/src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from "@config/auth";
+import AppError from '@shared/errors/AppError';
+
+interface TokenPayload {
+    iat: number,
+    exp: number,
+    sub: string,
+};
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+    //Validação do token JWT
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError('JWT token is missing', 401);
+    }
+
+    //Desestruturou o array, assim oq for separado pelo slip vai estar dentro dos obj do array
+    const [, token] = authHeader.split(' ');
+
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+        id: sub,
+    };
+
+    return next();
+
+}
